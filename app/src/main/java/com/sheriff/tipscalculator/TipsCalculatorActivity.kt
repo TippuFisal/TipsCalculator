47c18340225c4b678f75bf2b0f8fed553ff59d74
Tippu Fisal <tippufisal@vajro.com>
2021-11-05 09:04:14 +0530

    *Implemented Tips Calculator

--- app/src/main/java/com/sheriff/tipscalculator/TipsCalculatorActivity.kt
+++ app/src/main/java/com/sheriff/tipscalculator/TipsCalculatorActivity.kt
@@ -1,24 +1,52 @@
 package com.sheriff.tipscalculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.sheriff.tipscalculator.databinding.ActivityTipsCalculatorBinding
+import java.text.NumberFormat
+import kotlin.math.cos
 
 class TipsCalculatorActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityTipsCalculatorBinding
+    lateinit var binding: ActivityTipsCalculatorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTipsCalculatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
     }
 
-    private fun initUI(){
-
+    private fun initUI() {
+        calculateTips()
     }
 
-    private fun calculateTips(){
-
+    private fun calculateTips() {
+        binding.btnCalculate.setOnClickListener {
+            val stringInTextField = binding.etCostOfService.text.toString()
+            val cost = stringInTextField.toDoubleOrNull()
+            when{
+                cost == null ->{
+                    binding.tvTipsAmount.text = ""
+                    return@setOnClickListener
+                }
+            }
+            val selectedId = binding.rgTipsPercentage.checkedRadioButtonId
+            val tipPercentage = when(selectedId){
+                R.id.rbAmazing -> 0.20
+                R.id.rbGood -> 0.18
+                R.id.rbOkay -> 0.15
+                else -> 0.00
+            }
+            var tipAmount = tipPercentage * cost!!
+            val roundUp = binding.switchToRoundTips.isChecked
+            when(roundUp){
+                true -> {
+                    tipAmount = kotlin.math.ceil(tipAmount)
+                }
+            }
+            val finalTipAmount = NumberFormat.getCurrencyInstance().format(tipAmount)
+            binding.tvTipsAmount.text = getString(R.string.tip_amount, finalTipAmount)
+        }
     }
 }
